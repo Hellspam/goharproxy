12636a4d5038cb2aea0eb4fc0496593113a69315
hellspam <royarnon@gmail.com>
2014-07-10 11:09:03 +0300

    Testing harproxy in go routine.

--- src/main.go
+++ src/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "harproxy"
+import (
+	"harproxy"
+	"time"
+)
 
 
 func main() {
 	harProxy := harproxy.NewHarProxy(9999)
-	harProxy.Start()
+	go func() {harProxy.Start()}()
+	time.Sleep(5 * time.Second)
+	harProxy.Stop()
+	harProxy.PrintEntries()
 }
 
 
